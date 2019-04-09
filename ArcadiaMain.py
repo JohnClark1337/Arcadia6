7ccdcdd3be6be0a993e4cb6dd21d1cb8ffc68d8f
JScofield <jdsco37@gmail.com>
2019-04-09 16:54:06 -0400

    Add files via upload
    Connected Icon image and description from XML and linked to changing of index in the list of applications. Modified the tab index.

--- ArcadiaMain.py
+++ ArcadiaMain.py
@@ -11,19 +11,54 @@ class ArcadiaPy(QtWidgets.QMainWindow, Arcadia6.Ui_MainWindow):
         self.btnExit.clicked.connect(QtWidgets.QApplication.instance().quit)
         self.tabOS.setCurrentIndex(0)
         self.lstWindows.addItems(antivirusNames)
-        self.lstWindows.setCurrentRow(3)
-        self.lstWindows.itemClicked.connect(popIcon)
+        self.lstWindows.setCurrentRow(0)
+        self.iconImage.setPixmap(PyQt5.QtGui.QPixmap("Icons/Arcadia.ico"))
+        self.lstWindows.currentItemChanged.connect(self.popWinSoft)
+        self.btnAntivirus.clicked.connect(lambda: self.softwareList(1))
+        self.btnAntimalware.clicked.connect(lambda: self.softwareList(2))
+        self.btnClean.clicked.connect(lambda: self.softwareList(3))
+        self.btnSetup.clicked.connect(lambda: self.softwareList(4))
+        self.btnTools.clicked.connect(lambda: self.softwareList(5))
     
 
-def popIcon(self):
-    root = ET.parse('Programs.xml').getroot()
-    print("Hello World")
-    # for item in root.findall('Item'):
-    #     if item.get('name') ==  name:
-    #         pix = PyQt5.QtGui.QPixmap(item.find('Icon').text)
-    #         self.iconImage.setPixmap(pix)
-            
+    def clearLists(self):
+        self.lstWindows.clear()
+        self.lstLinux.clear()
+        self.lstMac.clear()
 
+
+    def softwareList(self, category):
+        self.clearLists()
+        if category == 1:
+            self.lstWindows.addItems(antivirusNames)
+        elif category == 2:
+            self.lstWindows.addItems(antimalNames)
+        elif category == 3:
+            self.lstWindows.addItems(cleanNames)
+        elif category == 4:
+            self.lstWindows.addItems(setupNames)
+        elif category == 5:
+            self.lstWindows.addItems(toolNames)
+        else:
+            self.lstWindows.addItems(antivirusNames)
+        self.lstWindows.setCurrentRow(0)
+
+
+    def popWinSoft(self):
+        try:
+            itemName = self.lstWindows.currentItem().text()
+            root = ET.parse('Programs.xml').getroot()
+            #print(itemName)
+            
+            for item in root.findall('Item'):
+                if item.get('name') ==  itemName:
+                    pix = PyQt5.QtGui.QPixmap(item.find('Icon').text)
+                    self.iconImage.setPixmap(pix)
+                    self.txtDescription.setText(item.find('Description').text)
+        except:
+            pix = PyQt5.QtGui.QPixmap("Icons/Arcadia.ico")
+            self.iconImage.setPixmap(pix)
+            
 
 antivirusNames = list()
 antimalNames = list()
